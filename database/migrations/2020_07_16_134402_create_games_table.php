00aab103dbcc983527bcd012f7c2e6c4e03fa8ae
mdm106 <mads.mcmahon@googlemail.com>
2020-07-16 16:07:19 +0100

    Migration updated for score to be an integer

--- database/migrations/2020_07_16_134402_create_games_table.php
+++ database/migrations/2020_07_16_134402_create_games_table.php
@@ -15,7 +15,7 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->enum("score", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            $table->integer("score");
             $table->timestamps();
         });
     }

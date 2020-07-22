03baf14b7d3f9b8bbbd5af2cb922b279b31a1ea9
mdm106 <mads.mcmahon@googlemail.com>
2020-07-22 10:02:31 +0100

    Added 15 to enum on questions table

--- database/migrations/2020_07_02_093740_create_questions_table.php
+++ database/migrations/2020_07_02_093740_create_questions_table.php
@@ -16,7 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('content', 200);
-            $table->enum('difficulty', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+            $table->enum('difficulty', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
             $table->string("amount");
             $table->timestamps();
         });
